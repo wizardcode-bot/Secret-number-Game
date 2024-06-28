a66c9206111c8a7f8f35106068adbc9894734c7e
Edinson <77586479+wizardcode-bot@users.noreply.github.com>
2024-06-28 12:03:32 -0500

    Update app.js
    Implemented the maximum amount of attempts, enable and disable the buttons when necessary.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let numeroSecreto = 0;
 let intentos = 0;
-let listaNumerosSorteados = [];
-let numeroMaximo = 10;
+let listaNumerosSorteados = [];//Lista para guardar los números ya sorteados, se reinicia el reiniciar la página
+let numeroMaximo = 50; //Límite para la generació del número secreto
+let numeroMaximoIntentos = 15;//Máximo de intentos que tiene el jugador
 
 
 
@@ -13,20 +14,28 @@ function asignarTextoElemento(elemento, texto) {
 
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    numeroMaximoIntentos--;
     
-    if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
-        document.getElementById('reiniciar').removeAttribute('disabled');
-    } else {
-        //El usuario no acertó.
-        if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p','El número secreto es menor');
+    if(numeroMaximoIntentos > 0){
+        if (numeroDeUsuario === numeroSecreto) {
+            asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+            document.getElementById('reiniciar').removeAttribute('disabled');//se habilita el botón de reinicar
+            deshabilitarIntentos();
         } else {
-            asignarTextoElemento('p','El número secreto es mayor');
+            //El usuario no acertó.
+            if (numeroDeUsuario > numeroSecreto) {
+                asignarTextoElemento('p',`El número secreto es menor, ${(numeroMaximoIntentos > 1) ? 'te quedan' : 'te queda'} ${numeroMaximoIntentos} ${(numeroMaximoIntentos > 1) ? 'intentos' : 'intento'}`);
+            } else {
+                asignarTextoElemento('p',`El número secreto es mayor, ${(numeroMaximoIntentos > 1) ? 'te quedan' : 'te queda'} ${numeroMaximoIntentos} ${(numeroMaximoIntentos > 1) ? 'intentos' : 'intento'}`);
+            }
+            intentos++;
+            limpiarCaja();
         }
-        intentos++;
-        limpiarCaja();
-    }
+    } else{
+        asignarTextoElemento('p', 'Te has quedado sin intentos!');
+        deshabilitarIntentos();
+        document.querySelector('#reiniciar').removeAttribute('disabled');//se habilita el botón de reiniciar juego
+    }    
     return;
 }
 
@@ -37,7 +46,7 @@ function limpiarCaja() {
 function generarNumeroSecreto() {
     let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
 
-    console.log(numeroGenerado);
+    // console.log(numeroGenerado);
     console.log(listaNumerosSorteados);
     //Si ya sorteamos todos los números
     if (listaNumerosSorteados.length == numeroMaximo) {
@@ -58,6 +67,7 @@ function condicionesIniciales() {
     asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
+    numeroMaximoIntentos = 15;
     console.log(numeroSecreto);
 }
 
@@ -69,8 +79,14 @@ function reiniciarJuego() {
     //Inicializar el número intentos
     condicionesIniciales();
     //Deshabilitar el botón de nuevo juego
-    document.querySelector('#reiniciar').setAttribute('disabled','true');
-    
+    document.querySelector('#reiniciar').setAttribute('disabled','true');//se deshabilita el botón de reiniciar al darle click y haber iniciado el nuevo juego
+    document.querySelector('#intentar').removeAttribute('disabled');//se vuelve a activar el botón de intentar
+    document.getElementById('valorUsuario').removeAttribute('disabled');//se vuelve a activar el campo para ingresar los números
+}
+
+function deshabilitarIntentos() {
+    document.getElementById('valorUsuario').setAttribute('disabled', 'true');//se deshabilita la caja de texto
+    document.getElementById('intentar').setAttribute('disabled','true');//se deshabilita el botón de intentos
 }
 
 condicionesIniciales();
